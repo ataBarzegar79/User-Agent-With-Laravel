bd1a0622ec295ef06f27c64c17d2534f3dfb3ee7
Ehsan Aghayari <ehsanaghayari313@gmail.com>
2022-10-13 13:37:41 +0330

    Get all userAgents with mobile deviceCategory

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user-agent', [UserAgentController::class, 'index'])->name('user-agent');
