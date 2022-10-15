c2191206a94084a599c84920070d4c83080736d6
Ehsan Aghayari <ehsanaghayari313@gmail.com>
2022-10-15 13:45:41 +0330

    User Agent Test Bugfix & Refactoring

--- tests/Feature/UserAgentTest.php
+++ tests/Feature/UserAgentTest.php
@@ -2,16 +2,20 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Arr;
 use Tests\TestCase;
 
 class UserAgentTest extends TestCase
 {
-    // Fetch All
-    public function testFetchAllResponseJsonMustBeInCorrectStructure()
+    // Fetch a Single User Agent
+    public function testFetchSingleUserAgentResponseStatusMustBeOk()
     {
-        $response = $this->getJson(route('user-agent'))
-            ->assertOk();
+        $this->getJson(route('user-agent'))->assertOk();
+    }
+
+    public function testFetchSingleUserAgentResponseJsonMustBeInCorrectStructure()
+    {
+        $response = $this->getJson(route('user-agent'));
         $response->assertJsonStructure(
             [
                 "user_agent" => [],
@@ -19,27 +23,74 @@ class UserAgentTest extends TestCase
         );
     }
 
-    public function testFetchAllUserAgentsResponseJsonMustNotBeNull()
+    public function testFetchSingleUserAgentResponseJsonMustNotBeNull()
     {
         $response = $this->getJson(route('user-agent'));
         $this->assertNotNull($response["user_agent"]);
     }
-    // End of Fetch All
+    // End of Fetch a Single User Agent
 
     // Device Filter
-    // todo: validations test
+    public function testFetchWithDeviceFilterResponseJsonMustNotBeNull()
+    {
+        $deviceCategories = ['mobile', 'tablet', 'desktop'];
+        $deviceCategory = Arr::random($deviceCategories);
+        $response = $this->getJson(route('user-agent', ['device' => $deviceCategory]));
+        $response->assertJsonStructure(
+            [
+                "user_agent" => [],
+            ]
+        );
+        $this->assertNotNull($response["user_agent"]);
+    }
+
+    public function testFetchWithDeviceFilterAndIncorrectDevice()
+    {
+        $deviceCategory = 'random';
+        $this->getJson(route('user-agent', ['device' => $deviceCategory]))
+            ->assertUnprocessable();
+    }
+
+    public function testFetchWithDeviceFilterAndIncorrectDeviceDataType()
+    {
+        $deviceCategories = [false, 100];
+        $deviceCategory = Arr::random($deviceCategories);
+        $this->getJson(route('user-agent', ['device' => $deviceCategory]))
+            ->assertUnprocessable();
+    }
     // End of Device Filter
 
     // Quantity Filter
     public function testFetchWithQuantityFilterResponseJsonItemsCountMustBeCorrect()
     {
-        $count = rand(1, 100);
+        $count = rand(1, 1000);
         $response = $this->getJson(route('user-agent', ['quantity' => $count]));
         $this->assertCount($count, $response["user_agent"]);
     }
-    // todo: validations test 0 , out of range , incorrect type , Minus
+
+    public function testFetchWithQuantityFilterAndIncorrectQuantity()
+    {
+        $countArr = [0, -1, 1001];
+        $count = Arr::random($countArr);
+        $this->getJson(route('user-agent', ['quantity' => $count]))
+            ->assertUnprocessable();
+    }
     // End of Quantity Filter
 
     // Both Quantity & Device Filter
+    public function testFetchWithBothDeviceAndQuantityFiltersResponseJsonMustBeInCorrectStructureCorrectDataCount()
+    {
+        $count = rand(1, 1000);
+        $deviceCategories = ['mobile', 'tablet', 'desktop'];
+        $deviceCategory = Arr::random($deviceCategories);
+        $response = $this->getJson(route('user-agent', ['quantity' => $count, 'device' => $deviceCategory]));
+        $response->assertJsonStructure(
+            [
+                "user_agent" => [],
+            ]
+        );
+
+        $this->assertCount($count, $response["user_agent"]);
+    }
     // End of Quantity Filter
 }
