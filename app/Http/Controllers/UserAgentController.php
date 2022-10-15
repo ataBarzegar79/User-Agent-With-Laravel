20e3e5006309800a6f6544df29f384303f14cde4
Ehsan Aghayari <ehsanaghayari313@gmail.com>
2022-10-15 09:31:50 +0330

    Completing Logic in User Agent Controller

--- app/Http/Controllers/UserAgentController.php
+++ app/Http/Controllers/UserAgentController.php
@@ -3,21 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class UserAgentController extends Controller
 {
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
         $json = json_decode(file_get_contents(base_path() . "/resources/src/user-agents.json"), true);
-        $nj = [];
-        foreach ($json as $j) {
-            if ($j["deviceCategory"] === 'mobile') {
-                $nj["user_agent"][] = $j["userAgent"];
+        $helperArray["user_agent"] = [];
+        $finalArray["user_agent"] = [];
+
+        // fetching random user agents in ordered quantity and related to specific deviceCategory
+        if ($request->device !== null && $request->quantity !== null) {
+            foreach ($json as $j) {
+                if ($j["deviceCategory"] === $request->device) {
+                    $helperArray["user_agent"][] = $j["userAgent"];
+                }
+            }
+
+            $rand = array_rand($helperArray["user_agent"], $request->quantity);
+            foreach ((array)$rand as $r) {
+                $finalArray["user_agent"][] = $helperArray["user_agent"][$r];
+            }
+
+
+            // fetching all user agents related to specific deviceCategory
+        } elseif ($request->device !== null) {
+            foreach ($json as $j) {
+                if ($j["deviceCategory"] === $request->device) {
+                    $finalArray["user_agent"][] = $j["userAgent"];
+                }
+            }
+
+
+            // fetching random user agents in ordered quantity
+        } elseif
+        ($request->quantity !== null) {
+            foreach ($json as $j) {
+                $helperArray["user_agent"][] = $j["userAgent"];
+            }
+            $rand = array_rand($helperArray["user_agent"], $request->quantity);
+            foreach ((array)$rand as $r) {
+                $finalArray["user_agent"][] = $helperArray["user_agent"][$r];
+            }
+
+
+            // fetching all user agents
+        } else {
+            foreach ($json as $j) {
+                $finalArray["user_agent"][] = $j["userAgent"];
             }
         }
 
+
         return response()->json(
-            $nj
+            $finalArray
         );
     }
 }
